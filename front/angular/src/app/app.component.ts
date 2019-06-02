35bd5bcb273b5dc720ce341c8c9bbf9ba6860461
velrino <denismagalhaesvelrino@gmail.com>
2019-06-02 11:04:37 -0300

    ngRepeat para exibir o catalogo de filmes

--- front/angular/src/app/app.component.ts
+++ front/angular/src/app/app.component.ts
@@ -8,4 +8,55 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class AppComponent {
   title = 'nerdflix';
+  private movies = [
+    {
+      id: 1,
+      name: 'Lorem Ipsum',
+      img: null,
+      text: null,
+    },
+    {
+      id: 2,
+      name: 'Lorem Ipsum',
+      img: null,
+      text: null,
+    },
+    {
+      id: 3,
+      name: 'Lorem Ipsum',
+      img: null,
+      text: null,
+    },
+    {
+      id: 4,
+      name: 'Lorem Ipsum',
+      img: null,
+      text: null,
+    },
+    {
+      id: 5,
+      name: 'Lorem Ipsum',
+      img: null,
+      text: null,
+    }
+  ]
+  public catalog = {
+    bests: [
+
+    ],
+    others: [
+      {
+        category: 'Ação',
+        movies: this.movies
+      },
+      {
+        category: 'Aventura',
+        movies: this.movies
+      },
+      {
+        category: 'Drama',
+        movies: this.movies
+      }
+    ]
+  }
 }
