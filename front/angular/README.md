867fb84a69b920e6927305e691a5ed007ee4fa1c
velrino <denismagalhaesvelrino@gmail.com>
2019-08-09 23:12:29 -0300

    Implements angular

--- front/angular/README.md
+++ front/angular/README.md
@@ -1,6 +1,6 @@
 # Nerdflix
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.7.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.1.
 
 ## Development server
 
--- front/angular/src/app/app.component.ts
+++ front/angular/src/app/app.component.ts
@@ -1,62 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'nerdflix';
-  private movies = [
-    {
-      id: 1,
-      name: 'Lorem Ipsum',
-      img: null,
-      text: null,
-    },
-    {
-      id: 2,
-      name: 'Lorem Ipsum',
-      img: null,
-      text: null,
-    },
-    {
-      id: 3,
-      name: 'Lorem Ipsum',
-      img: null,
-      text: null,
-    },
-    {
-      id: 4,
-      name: 'Lorem Ipsum',
-      img: null,
-      text: null,
-    },
-    {
-      id: 5,
-      name: 'Lorem Ipsum',
-      img: null,
-      text: null,
-    }
-  ]
-  public catalog = {
-    bests: [
-
-    ],
-    others: [
-      {
-        category: 'Ação',
-        movies: this.movies
-      },
-      {
-        category: 'Aventura',
-        movies: this.movies
-      },
-      {
-        category: 'Drama',
-        movies: this.movies
-      }
-    ]
-  }
 }
