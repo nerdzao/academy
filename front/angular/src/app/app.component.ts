3355e37820f864c73cbcfc507ba73c7a74897335
velrino <denismagalhaesvelrino@gmail.com>
2019-08-18 23:03:57 -0300

    Refactory Nerdflix

--- front/angular/src/app/app.component.ts
+++ front/angular/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nerdflix';
+  showMovie = {
+    index: null,
+    movie: null,
+  }
+  data = [
+    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+  ]
+
+  selected(index, movie) {
+    this.showMovie = { index, movie }
+  }
 }
