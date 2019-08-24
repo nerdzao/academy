df0afb98626a2e6165b3ba5ceb39319374748cf5
velrino <denismagalhaesvelrino@gmail.com>
2019-08-24 18:04:50 -0300

    Improvements without layout

--- front/angular/src/app/app.component.ts
+++ front/angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,18 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nerdflix';
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+  constructor(config: NgbCarouselConfig, private elRef: ElementRef) {
+    config.interval = 100000000000000000000;
+    config.pauseOnHover = true;
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+  }
+
   showMovie = {
     index: null,
     movie: null,
   }
+
   data = [
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}],
+    {
+      type: "Lançamentos",
+      movies: [
+        {
+
+        },
+      ],
+    },
+    {
+      type: "Comédia",
+      movies: [
+        {
+
+        }
+      ],
+    },
+    {
+      type: "Ação",
+      movies: [
+        {
+          movie: ""
+        }
+      ],
+    }
   ]
 
-  selected(index, movie) {
-    this.showMovie = { index, movie }
+  selected(index: number, movie: object) {
+    this.showMovie = !this.hasShowMovie(index) ? { index, movie } : { index: null, movie: null };
+  }
+
+  hasShowMovie(i: number) {
+    return (this.showMovie.index == i);
   }
 }
